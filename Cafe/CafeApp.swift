78fe90adabef7c456ba40dd22408d03fda2623f6
dcas796 <89193854+dcas796@users.noreply.github.com>
2024-02-07 18:53:45 +0100

    Initial Commit

---
+++ Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by Dani on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
