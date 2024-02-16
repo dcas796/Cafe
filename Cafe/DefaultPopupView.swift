7b52ca829ceaee54104ca3098817d296c8313cbe
dcas796 <89193854+dcas796@users.noreply.github.com>
2024-02-16 21:22:48 +0100

    Added animation to title in `DefaultPopupView`

--- Cafe/DefaultPopupView.swift
+++ Cafe/DefaultPopupView.swift
@@ -10,17 +10,36 @@ import SwiftUI
 struct DefaultPopupView: View {
     @EnvironmentObject var model: ModelData
     
+    let titleAnimationOffset: CGFloat = 15
+    let animationDuration: CGFloat = 0.2
+    
     var body: some View {
         ZStack(alignment: .topTrailing) {
             VStack(spacing: 20) {
-                Text(model.isActive ? "Active" : "Inactive")
-                    .font(.title3)
-                    .bold()
+                ZStack {
+                    Text("Active")
+                        .font(.title3)
+                        .bold()
+                        .offset(x: 0, y: model.isActive ? 0 : -titleAnimationOffset)
+                        .opacity(model.isActive ? 1 : 0)
+                    
+                    Text("Inactive")
+                        .font(.title3)
+                        .bold()
+                        .offset(x: 0, y: model.isActive ? titleAnimationOffset : 0)
+                        .opacity(model.isActive ? 0 : 1)
+                }
+                .animation(.smooth(duration: animationDuration), value: model.isActive)
+                
+                Spacer()
                 
                 Image(systemName: model.isActive ? "cup.and.saucer.fill" : "cup.and.saucer")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(height: 50)
+                    .animation(.smooth(duration: animationDuration), value: model.isActive)
+                
+                Spacer()
                 
                 VStack {
                     Toggle(isOn: $model.isActive) {
